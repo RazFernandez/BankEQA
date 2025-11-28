0890d8e3f21f43ce2fe3b19ef1870324e63d40d9
RazFernandez <mr.fernandez.mx@outlook.com>
2025-11-27 20:24:01 -0600

    Initialized Java Backend Project

---
+++ Backend/bankr/src/main/java/com/example/bankr/BankrApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankrApplication.class, args);
+	}
+
+}
---
+++ Backend/bankr/src/test/java/com/example/bankr/BankrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
